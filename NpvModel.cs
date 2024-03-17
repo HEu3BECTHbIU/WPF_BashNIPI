2351258088ef3ff528b3586f4a708928781fc88c
Demid <90619714+HEu3BECTHbIU@users.noreply.github.com>
2024-03-17 22:20:07 +0500

    Add files via upload

---
+++ NpvModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CountNPV
+{
+    internal static class NpvModel
+    {
+        public static double CountNPV(double discount, int year)
+        {
+            string filename = "data.xlsx";
+            string path = Path.GetFullPath(filename);
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(path);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[2];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            object[,] valueArray = (object[,])xlRange.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+
+            double npv = 0;
+            int row = 2;
+            while (Convert.ToInt32(valueArray[row, 1]) < year)
+            {
+                int cf = Convert.ToInt32(valueArray[row, 2]) - Convert.ToInt32(valueArray[row, 3]);
+                npv += (double)cf * (1 / Math.Pow((1.0 + discount), row - 1));
+                row++;
+            }
+            int cf_last = Convert.ToInt32(valueArray[row, 2]) - Convert.ToInt32(valueArray[row, 3]);
+            npv += (double)cf_last * (1 / Math.Pow((1.0 + discount), row - 1));
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            return npv;
+        }
+    }
+}
---
+++ NpvVM.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CountNPV
+{
+    public class NpvVM : INotifyPropertyChanged
+    {
+        private string year = "2050";
+        private string discount = "0.2";
+        private double npv;
+        public string Year { get { return year; } set { year = value;  } }
+
+        public string Discount { 
+            get 
+            {
+                return discount; 
+            } set { discount = value; } }
+        public double NPV { get { return Math.Round(npv, 2); } private set { npv = value;  } }
+
+        private Command countNPV;
+        public Command CountNPV 
+        {
+            get
+            {
+                return countNPV ??
+                    (countNPV = new Command(obj =>
+                    {
+                        IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+                        if (double.TryParse(Discount, NumberStyles.AllowDecimalPoint, formatter, out double result))
+                        {
+                            if (result < 0)
+                            {
+                                MessageBox.Show("Ставка дисконтирования указана в неверном формате");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ставка дисконтирования указана в неверном формате");
+                            return;
+                        }
+                        if (Int32.TryParse(Year.ToString(), out int year))
+                        {
+                            if (year < 2020 || year > 2050)
+                            {
+                                MessageBox.Show("Год указан неверно, возможные значения: 2020 - 2050");
+                                return;
+                            }
+                        }
+                        
+                        NPV = NpvModel.CountNPV(Double.Parse(Discount, NumberStyles.AllowDecimalPoint, formatter), Int32.Parse(Year));
+                        OnPropertyChanged("NPV");
+                    }));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        internal void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
